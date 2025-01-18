cae56083dcd296b6db54f7f3d2eb3940c3b9eeb0
Hamza <hamzabenmammar2020@gmail.com>
2025-01-18 23:00:00 +0100

    hamza

--- projet_si/app1/urls.py
+++ projet_si/app1/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
 
     path('',accueil_view.accueil, name='accueil'),
     
-    # Route pour afficher la liste des employés
+    # Route pour gerer les employés
      path('employe/recherche/', employe_views.rechercher_employe, name='rechercher_employe'),
     path('employe/insert/', employe_views.insert_employe, name='insert_employe'),
     path('employe/edit/<int:employe_id>/', employe_views.modifier_employe, name='modifie_employe'),
@@ -35,6 +35,7 @@ urlpatterns = [
     path('conges/delete/<int:conge_id>/', conge_views.supprime_conge, name='delete_conge'),
     
 
+# Route pour gerer les salaires
     path('salaires/', salaire_views.gestion_salaire, name='gestion_salaire'),
     path('salaires/<int:salaire_id>/consulter/', salaire_views.consulter_salaire, name='consulter_salaire'),
     path('salaires/ajouter/', salaire_views.ajouter_salaire, name='ajouter_salaire'),
@@ -42,23 +43,30 @@ urlpatterns = [
     path('salaires/<int:salaire_id>/supprimer/', salaire_views.supprimer_salaire, name='supprimer_salaire'),
     path('salaires/<int:employe_id>/export_pdf/', export_pdf, name='export_pdf'),
 
+# Route pour gerer les absences
     path('ajouter/absence/', absence_views.ajouter_absence, name='ajouter_absence'),
     path('supprimer/absence/<int:pk>/', absence_views.supprimer_absence, name='supprimer_absence'),
     path('modifier/absence/<int:pk>/', absence_views.modifier_absence, name='modifier_absence'),
     path('consulter/absence/<int:id>/', absence_views.consulter_absence, name='consulter_absence'),
     path('gestion/absences/', absence_views.gestion_absence, name='gestion_absence'),
 
+
+# Route pour gerer les primes
     path('ajouter/prime/', prime_views.ajouter_prime, name='ajouter_prime'),
     path('supprimer/prime/<int:pk>/', prime_views.supprimer_prime, name='supprimer_prime'),
     path('modifier/prime/<int:pk>/', prime_views.modifier_prime, name='modifier_prime'),
     path('consulter/primes/', prime_views.consulter_primes, name='consulter_primes'),
     path('gestion/primes/', prime_views.gestion_prime, name='gestion_prime'),
 
+
+# Route pour gerer les masroufs
     path('ajouter/masrouf/', masrouf_views.ajouter_masrouf, name='ajouter_masrouf'),
     path('supprimer/masrouf/<int:pk>/', masrouf_views.supprimer_masrouf, name='supprimer_masrouf'),
     path('modifier/masrouf/<int:pk>/', masrouf_views.modifier_masrouf, name='modifier_masrouf'),
     path('gestion/masroufs/', masrouf_views.gestion_masrouf, name='gestion_masrouf'),
 
+
+# Route pour gerer les contrats
     path('contrats/', contrat_views.recherche_contrats, name='recherche_contrats'),
     path('contrats/ajouter/', contrat_views.ajouter_contrat, name='ajouter_contrat'),
     path('contrats/<int:contrat_id>/', contrat_views.details_contrat, name='details_contrat'),
@@ -66,7 +74,7 @@ urlpatterns = [
     path('contrat/delete/<int:contrat_id>/', contrat_views.supprimer_contrat, name='supprimer_contrat'),
     
     
-    
+    # Route pour gerer les recrutements
     path('create-job-offer/', recrutement_views.create_job_offer, name='create_job_offer'),
     path('job-offers/', recrutement_views.job_offers_list, name='job_offers_list'),
     path('apply/<int:job_offer_id>/', recrutement_views.apply_for_job, name='apply_for_job'),
@@ -74,20 +82,21 @@ urlpatterns = [
     path('schedule-interview/<int:application_id>/', recrutement_views.schedule_interview, name='schedule_interview'),
     path('interview-details/<int:interview_id>/', recrutement_views.interview_details, name='interview_details'),
     
+    # Route pour gerer les services
     path('service/', service_views.recherche_service, name='recherche_service'),
     path('service/insert/', service_views.insert_service, name='insert_service'),
     path('service/edit/<int:service_id>/', service_views.modifie_service, name='modifie_service'),
     path('service/delete/<int:service_id>/', service_views.supprime_service, name='supprime_service'),
     path('service/consult/<int:service_id>/', service_views.consult_service, name='consult_service'),
 
-
+# Route pour gerer les evaluations
     path('evaluations/', evaluation_views.gestion_evaluations, name='gestion_evaluations'),
     path('evaluations/ajouter/', evaluation_views.ajouter_evaluation, name='ajouter_evaluation'),
     path('evaluations/modifier/<int:pk>/', evaluation_views.modifier_evaluation, name='modifier_evaluation'),
     path('evaluations/supprimer/<int:pk>/', evaluation_views.supprimer_evaluation, name='supprimer_evaluation'),
     path('evaluations/consulter/<int:pk>/', evaluation_views.consulter_evaluation, name='consulter_evaluation'),
 
-
+# Route pour gerer l'analyse
     path('analyse_effectifs/', analyse_views.analyse_effectifs, name='analyse_effectifs'),
     path('repartition_employes/', analyse_views.repartition_employes, name='repartition_employes'),
     path('top-performeurs/', analyse_views.top_performeurs, name='top_performeurs'),
